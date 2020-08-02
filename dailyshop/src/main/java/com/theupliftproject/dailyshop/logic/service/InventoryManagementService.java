133ac12c3975ba2f1ed49ae45d27198de7c61b62
Priya Raut <rautpriya215@gmail.com>
2020-08-02 22:37:23 +0530

    Add pagination and sorting using JpaRepository.

--- dailyshop/src/main/java/com/theupliftproject/dailyshop/logic/service/InventoryManagementService.java
+++ dailyshop/src/main/java/com/theupliftproject/dailyshop/logic/service/InventoryManagementService.java
@@ -2,7 +2,14 @@ package com.theupliftproject.dailyshop.logic.service;
 
 import com.theupliftproject.dailyshop.data.entity.Item;
 import com.theupliftproject.dailyshop.data.repository.ItemsRepository;
+import com.theupliftproject.dailyshop.web.InventoryController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -13,6 +20,7 @@ import java.util.List;
  */
 @Service
 public class InventoryManagementService {
+    public static final Logger LOGGER = LoggerFactory.getLogger(InventoryManagementService.class);
     private final ItemsRepository itemsRepository;
 
     @Autowired
@@ -25,12 +33,18 @@ public class InventoryManagementService {
      *
      * @return list of items to being sold at daily shop
      */
-    public List<Item> getAllItems(){
-        List<Item> items = new ArrayList<>();
-        Iterable<Item> iterableItems = this.itemsRepository.findAll();
-        for(Item item : iterableItems){
-            items.add(item);
-        }
-        return items;
+    public List<Item> getAllItems(int page, int size){
+        LOGGER.info("Inside getAllItems.");
+        // with pagination
+        Pageable pageable = PageRequest.of(page, size, Sort.by("name"));
+        Page<Item> itemsPage = this.itemsRepository.findAll(pageable);
+        // without pagination
+//        List<Item> items = new ArrayList<>();
+//        Iterable<Item> iterableItems = this.itemsRepository.findAll();
+//        for(Item item : iterableItems){
+//            items.add(item);
+//        }
+//        return items;
+        return itemsPage.getContent();
     }
 }
--- dailyshop/src/main/java/com/theupliftproject/dailyshop/web/InventoryController.java
+++ dailyshop/src/main/java/com/theupliftproject/dailyshop/web/InventoryController.java
@@ -1,12 +1,17 @@
 package com.theupliftproject.dailyshop.web;
 
+import com.theupliftproject.dailyshop.DailyShopApplication;
 import com.theupliftproject.dailyshop.data.entity.Item;
 import com.theupliftproject.dailyshop.logic.service.InventoryManagementService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -15,6 +20,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/inventory-management")
 public class InventoryController {
+    public static final Logger LOGGER = LoggerFactory.getLogger(InventoryController.class);
+
     private final InventoryManagementService inventoryManagementService;
 
     @Autowired
@@ -24,7 +31,10 @@ public class InventoryController {
 
     @GetMapping
     @RequestMapping("/items")
-    public List<Item> getAllItems(){
-        return this.inventoryManagementService.getAllItems();
+    public List<Item> getAllItems(@RequestParam(name = "page") int page, @RequestParam(name = "size") int size){
+        LOGGER.info("Inside getAllItems");
+        List<Item> items = this.inventoryManagementService.getAllItems(page, size);
+        if(items != null && !items.isEmpty()) return items;
+        else return new ArrayList<>(); // return error
     }
 }
