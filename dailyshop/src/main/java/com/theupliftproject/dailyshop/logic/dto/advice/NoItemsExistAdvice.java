ead1f25a89d810c23cc688e49f18f01488302e75
Priya Raut <rautpriya215@gmail.com>
2020-08-05 13:21:07 +0530

    Update NoItemsExistAdvice.java

--- dailyshop/src/main/java/com/theupliftproject/dailyshop/logic/dto/advice/NoItemsExistAdvice.java
+++ dailyshop/src/main/java/com/theupliftproject/dailyshop/logic/dto/advice/NoItemsExistAdvice.java
@@ -5,21 +5,21 @@ import com.theupliftproject.dailyshop.web.InventoryController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
-@ControllerAdvice
+import java.util.HashMap;
+
+@RestControllerAdvice
 public class NoItemsExistAdvice {
     public static final Logger LOGGER = LoggerFactory.getLogger(NoItemsExistAdvice.class);
 
-    @ResponseBody
     @ExceptionHandler(NoItemsExistsException.class)
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    String handleNoItemsExistException(NoItemsExistsException exception){
-
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public HashMap<String, String> handleNoItemsExistException(NoItemsExistsException exception){
         LOGGER.info("handleNoItemsExistException : " + exception.getMessage());
-       return exception.getMessage();
+        HashMap<String, String> response = new HashMap<>();
+        response.put("message", exception.getMessage());
+        response.put("error", exception.getClass().getSimpleName());
+        return response;
     }
 }
